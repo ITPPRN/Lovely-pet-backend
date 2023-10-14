89a2fb4761fc5ab31dd6fe9270196681f36410e3
อิทธิพล ประยูรเนตร <ittipol.pr@rmuti.ac.th>
2023-10-14 15:32:44 +0700

    edit send email2

--- target/generated-sources/annotations/com/example/lovelypet/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/lovelypet/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2566-10-14T15:12:49+0700",
+    date = "2566-10-14T15:30:26+0700",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.2 (Oracle Corporation)"
 )
 @Component
